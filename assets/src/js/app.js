18638acb173411dfa7125906426f1b8ef6aef48c
sarbjeet <sabi521@gmail.com>
2024-04-25 13:35:52 -0400

    home page updated

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -3,19 +3,44 @@
  * @param {function} fn
  * @return {void}
  */
-function domReady(fn)
-{
-    // If we're early to the party
-    document.addEventListener("DOMContentLoaded", fn);
-    // If late; I mean on time.
-    if (document.readyState === "interactive" || document.readyState === "complete" ) {
-        fn();
-    }
+function domReady(fn) {
+  // If we're early to the party
+  document.addEventListener("DOMContentLoaded", fn);
+  // If late; I mean on time.
+  if (
+    document.readyState === "interactive" ||
+    document.readyState === "complete"
+  ) {
+    fn();
+  }
 }
 
 /**
  * Application entrypoint
  */
 domReady(() => {
-    console.log('The Dom is ready! 🚀');
+  console.log("The Dom is ready! 🚀");
+  console.log("hello");
+
+  if (document.querySelector(".my-slider")) {
+    var slider = tns({
+      container: ".my-slider",
+      slideBy: "page",
+      autoplay: true,
+      controlsText: [
+        '<img src="/assets/src/images/next_icon.png" />',
+        '<img src="/assets/src/images/next_icon.png" />',
+      ],
+      loop: true,
+      mouseDrag: true,
+      gutter: 10,
+      controls: true,
+      fixedWidth: 86,
+      responsive: {
+        768: {
+          fixedWidth: 253,
+        },
+      },
+    });
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit', // activates Just-in-Time mode
-    prefix: '', // adds 'tw-' prefix to the classes
-    content: [
-        './**/*.html',
-        './**/*.{vue,js,ts,jsx,tsx}'
-    ], // update this path if needed, include *.php
-    media: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  mode: "jit", // activates Just-in-Time mode
+  prefix: "", // adds 'tw-' prefix to the classes
+  content: ["./**/*.html", "./**/*.{vue,js,ts,jsx,tsx}"], // update this path if needed, include *.php
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["ui-sans-serif", "system-ui"],
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["montserrat", "sans-serif"],
+        nexaheavy: ['"Nexa Heavy"'],
+        nexabold: ['"Nexa Bold"'],
+        nexaregular: ['"Nexa Regular"'],
+        nexalight: ['"Nexa Light"'],
+        kommissarCondensed: ['"Kommissar Condensed"'],
+        ubuntuRegular: ["Ubuntu Regular"],
+        ubuntuLight: ["Ubuntu Light"],
+        ubuntuBold: ["Ubuntu Bold"],
+        ubuntuMedium: ["Ubuntu Medium"],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
