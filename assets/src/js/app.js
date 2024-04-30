71d775b3b88b9ea1748341f2edc777b3e7d60a92
sarbjeet <sabi521@gmail.com>
2024-04-29 23:42:10 -0400

    new updates game page

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -20,7 +20,6 @@ function domReady(fn) {
  */
 domReady(() => {
   console.log("The Dom is ready! 🚀");
-  console.log("hello");
 
   if (document.querySelector(".my-slider")) {
     var slider = tns({
@@ -37,10 +36,97 @@ domReady(() => {
       controls: true,
       fixedWidth: 86,
       responsive: {
-        768: {
+        600: {
           fixedWidth: 253,
         },
       },
     });
   }
+
+  /**
+   * Video slider home page with thumbnails
+   */
+
+  //thumbnails
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 1.5,
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+
+    breakpoints: {
+      460: {
+        slidesPerView: 2.5,
+      },
+
+      768: {
+        slidesPerView: 3.5,
+      },
+      1024: {
+        slidesPerView: 4.5,
+      },
+    },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+  /**
+   * Basics container Slider
+   */
+
+  var swiper = new Swiper(".basicSwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    breakpoints: {
+      769: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 5,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  /**
+   * Featured Games container Slider
+   */
+
+  var swiper = new Swiper(".featuredSwiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 10,
+    breakpoints: {
+      769: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
